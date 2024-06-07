f058b356df44db95dd232175b68cf68e1d78c898
Jaxon7Cheung <2104526824@qq.com>
2024-06-07 23:18:19 +0800

    ARC单例

--- 单例传值/SingletonByARC.m
+++ 单例传值/SingletonByARC.m
@@ -9,4 +9,28 @@
 
 @implementation SingletonByARC
 
+static SingletonByARC* insVar = nil;
+
++ (instancetype)allocWithZone:(struct _NSZone *)zone {
+    if (!insVar) {
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            insVar = [super allocWithZone: zone];
+        });
+    }
+    return insVar;
+}
+
+- (instancetype)init {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        insVar = [super init];
+    });
+    return insVar;
+}
+
++ (instancetype)sharedInstance {
+    return [[self alloc] init];
+}
+
 @end
